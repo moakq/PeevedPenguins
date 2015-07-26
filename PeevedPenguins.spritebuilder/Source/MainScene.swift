befe63aaa41d97712dc6a0a6d457a7973ad60c66
Muhammed Othman <muhammed.ottman@gmail.com>
2015-07-25 21:42:12 -0700

    add button, gameplay scene

--- PeevedPenguins.spritebuilder/Source/MainScene.swift
+++ PeevedPenguins.spritebuilder/Source/MainScene.swift
@@ -1,5 +1,10 @@
 import Foundation
 
 class MainScene: CCNode {
+ 
+    func play() {
+        let gameplayScene = CCBReader.loadAsScene("Gameplay")
+        CCDirector.sharedDirector().presentScene(gameplayScene)
+    }
     
 }
