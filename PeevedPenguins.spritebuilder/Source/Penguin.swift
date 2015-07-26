643cd33e68b95ae39fa50e8c0f6e0e0973fc6c01
Muhammed Othman <muhammed.ottman@gmail.com>
2015-07-25 20:55:14 -0700

    add penguin, seal, and code connections

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.swift
@@ -0,0 +1,13 @@
+//
+//  Penguin.swift
+//  PeevedPenguins
+//
+//  Created by Muhammed Othman on 7/25/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class Penguin: CCSprite {
+
+}
---
+++ PeevedPenguins.spritebuilder/Source/Seal.swift
@@ -0,0 +1,13 @@
+//
+//  Seal.swift
+//  PeevedPenguins
+//
+//  Created by Muhammed Othman on 7/25/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import UIKit
+
+class Seal: CCSprite {
+
+}
